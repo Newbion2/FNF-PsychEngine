e06278d6229d12267210981eed94cb317178a799
Ejrjrlq <96973667+Newbion2@users.noreply.github.com>
2023-09-10 14:33:51 +0900

    Add files via upload

---
+++ assets/preload/stages/couch.lua
@@ -0,0 +1,9 @@
+function onCreate()
+	-- background shit
+	makeLuaSprite('couch', 'couch', -600, -300);
+	setScrollFactor('couch', 0.9, 0.9);
+
+
+	addLuaSprite('couch', false);
+	
+end
---
+++ assets/preload/stages/kitchen.lua
@@ -0,0 +1,9 @@
+function onCreate()
+	-- background shit
+	makeLuaSprite('kitchen', 'kitchen', -600, -300);
+	setScrollFactor('kitchen', 0.9, 0.9);
+
+
+	addLuaSprite('kitchen', false);
+	
+end
